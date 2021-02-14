733c93b0f31e908076f7c9a1908fe4f393a2cd19
João Ferreira <jferreira.biz@gmail.com>
2021-02-14 15:12:05 +0000

    Added the new phone field to ExternalLoginConfirmation and Register forms and respective classes. Moved ExternalLoginConfirmationViewModel and RegisterViewModels to their own respective seperate view models.

---
+++ Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.ViewModels
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        [Required]
+        [Display(Name = "Driving Licence")]
+        public string DrivingLicence { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
